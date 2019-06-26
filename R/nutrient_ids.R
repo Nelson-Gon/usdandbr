a34acaf4611bf01547e2f8fc316d347587ed9554
Nelson-Gon <gonzabato@hotmail.com>
2019-06-26 19:46:14 +0800

    Add data to ease NDBno access

---
+++ R/nutrient_ids.R
@@ -0,0 +1,11 @@
+#' A Curated list of nutrient IDs linked to Pubmed data.
+#' This data set contains nutrient_ids as provided by the USDA NDB.
+#' @format 'data.frame':	125 obs. of  4 variables:
+#' \describe{
+#' \item{PubChem.CID.s.}{Integer code corresponding to a PubChem ID}
+#' \item{NDB.ID}{The corresponding NDB nutrient id}
+#' \item{NDB.Name}{Nutrient's common name}
+#' 
+#' }
+#' @source \url{https://bmcresnotes.biomedcentral.com/articles/10.1186/s13104-018-3997-y}
+"nutrient_ids"
