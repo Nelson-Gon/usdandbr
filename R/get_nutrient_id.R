73cc550fdcff301c0393be4c7e4890b7d906f5ec
Nelson-Gon <gonzabato@hotmail.com>
2019-07-27 14:29:22 +0800

    Handle data missingness

--- R/get_nutrient_id.R
+++ R/get_nutrient_id.R
@@ -3,13 +3,17 @@
 #' by the USDA.
 #' @param nutrient_name Name of the nutrient for which an id is returned.
 #' @param df The data set that contains nutrient ids. Can be loaded into the 
-#' environment by calling `data("nutrient_ids")`
+#' environment by calling `data("nutrient_ids")`. If no data set is provided, the default is to 
+#' use `nutrient_ids`.
 #' @return Returns the nutrient id.
 #' @examples 
 #' data("nutrient_ids")
 #' get_nutrient_id("myri",nutrient_ids)
 #' @export
 get_nutrient_id<- function(nutrient_name,df){
+  if(missing(df)){
+    df <- get("nutrient_ids", envir=.GlobalEnv)
+  }
   df$NDB.Name <- sapply(df$NDB.Name,tolower)
   nutrient_name <- match.arg(nutrient_name,df$NDB.Name)
   to_match <- which(df$NDB.Name==nutrient_name)
