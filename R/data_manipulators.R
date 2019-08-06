db2571c67d48a704a79938145483b1e4adb4f231
Nelson-Gon <gonzabato@hotmail.com>
2019-08-07 01:34:32 +0800

    Support grouping, prettier pretty_json

--- R/data_manipulators.R
+++ R/data_manipulators.R
@@ -1,10 +1,37 @@
 #' Pretty JSON Lists
 #' @description A convenience function that returns processed JSON data.
-#' @param result An object obtained from `get_nutrients`
-#' @return A pretty `dput` style list.
+#' @param result An object obtained from `get_nutrient_info`
+#' @return A `data.frame` object with prettier JSON formatter data.
+#' @seealso \code{\link{get_nutrient_info}}
+#' @examples 
+#' \dontrun{
+#' # After setting api_key, get data
+#' res <-get_nutrients(nutrients=c("204","205"),
+#' result_type="json",
+#' offset = 25,max_rows = 50,food_group = c("0500","0100"))
+#' # use get_nutrient_info to get a semi-processed output
+#' res2<-get_nutrient_info(res)
+#' #prettify the above output
+#' pretty_json(res2)
+#' }
 #' @export
 pretty_json <- function(result) {
-  lapply(result, str)
+# This splits the data into the appropriate 
+# Values, Food_Type, ID, SOURCE
+pretty_result<- data.frame(Food=result[seq(2,
+                                  nrow(result),5),2],
+                    ID=result[seq(1,nrow(result),5),2],
+                    Source=result[seq(1,nrow(result),5),1])
+  numerics_only<-suppressWarnings(as.numeric(result$Value))
+ numerics_only<-numerics_only[!is.na(numerics_only)] 
+ 
+  pretty_result$Value<-numerics_only[seq(1,
+                    length(numerics_only),2)]
+  pretty_result$Value_2 <-numerics_only[seq(2,
+                              length(numerics_only),2)]
+  pretty_result$ID <- gsub("\\D","",pretty_result$ID)
+  pretty_result[,c(3,1,2,4,5)]
+  
 }
 #' Convert JSON data to a simpler data.frame object.
 #' @description When JSON data is requested, `get_nutrient info` provides
