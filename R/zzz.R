f25f5ce1e75005fd9ec1f1e7cb4a3c83c74b2822
Nelson-Gon <gonzabato@hotmail.com>
2019-08-20 16:19:32 +0800

    issues with build

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,4 @@
 # "startup" messages
-#' @importFrom  utils packageVersion
 .onAttach <- function(...){
 
    packageStartupMessage(startup_message())
@@ -8,7 +7,7 @@
 startup_message <- function(){
    issues_url <-"https://github.com/Nelson-Gon/usdar/issues" 
   paste0("Welcome to usdar version ",
-                                packageVersion("usdar"),".\nPlease set api key for this session with set_apikey",
+                              utils::packageVersion("usdar"),".\nPlease set api key for this session with set_apikey",
                                 "\nFor more information, please see :",gsub("/issues","",issues_url),
                                 "\nReport issues at: ", issues_url
    )
